da3b447a8999fc4ed80bac940c879cac916d39dc
Manal-Bukhari <manalbukhari2233@gmail.com>
2024-10-29 21:09:14 +0500

    Stucture

---
+++ METRO/src/Main.java
@@ -0,0 +1,9 @@
+
+import view.SplashView;
+
+public class Main {
+    public static void main(String[] args) {
+        SplashView splash = new SplashView();
+       // splash.showSplash();
+    }
+}
